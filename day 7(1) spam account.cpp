3d1f9fe5003d1a96a6864f67bfd3c4ba59269c31
ThatOneARYAN <131991633+ThatOneARYAN@users.noreply.github.com>
2023-09-24 18:28:03 +0530

    Create day 7(1) spam account.cpp

---
+++ day 7(1) spam account.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t,x,y;
+	cin>>t;
+	while(t--){ 
+     cin>>x>>y;
+     if(x>y*10){ 
+         cout<<"yes"<<endl;
+     }
+     else {
+         cout<<"no"<<endl;
+
+     }
+      	}
+	return 0;
+}
