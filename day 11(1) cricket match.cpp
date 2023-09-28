c01574b67d548757dca9388b8f5964d95de5faf6
ThatOneARYAN <131991633+ThatOneARYAN@users.noreply.github.com>
2023-09-28 20:45:54 +0530

    Create day 11(1) cricket match.cpp

---
+++ day 11(1) cricket match.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int T,N,M;
+    cin>>T;
+    while(T--)
+    {
+        cin>>N>>M;
+        if(N<=M*36)
+        cout<<"YES"<<endl;
+        else
+        cout<<"NO"<<endl;
+    }
+	return 0;
+}
