2f7b54d6138b88a99d2ec20200e8a2bc828b1ea9
ThatOneARYAN <131991633+ThatOneARYAN@users.noreply.github.com>
2023-09-30 20:16:59 +0530

    Create day 12(2) candy comp.cpp

---
+++ day 12(2) candy comp.cpp#include <iostream> #include <string> using namespace std;  int main() { 	/day 12(2) candy comp.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int a,b;
+	    cin>>a>>b;
+	    int count = 1,limak =0,bob=0;
+	    while(limak<=a||bob<=b){
+	        limak += count ;
+	        if(limak>a){
+	            cout<<"Bob"<<endl;
+	            break;
+	        }
+	        count++;
+	        bob += count ;
+	        if(bob>b){
+	            cout<<"Limak"<<endl;
+	            break;
+	        }
+	        count++;
+	    }
+	}
+	return 0;
+}
